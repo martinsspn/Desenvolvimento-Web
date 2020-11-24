6fb9f372556526b931b1fb82d100d7dd70c62754
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2020-11-24 16:24:30 -0300

    Create cadastro.js

---
+++ cadastro.js
@@ -0,0 +1,30 @@
+function cadastro(username, password) {
+    let rawdata = fs.readFileSync('users.json');
+    let creds = JSON.parse(rawdata);
+    let users = Object.keys(creds);
+    fs.writeFileSync('users.json', "{");
+    fs.appendFileSync('users.json', '\n');
+    for(var i = 0; i < users.length+1; i++) {
+        var person = creds[users[i]];
+        if(i === (users.length)){
+        	var name = {'username': username, 'password' : password}
+            fs.appendFileSync('users.json', JSON.stringify(username));
+            fs.appendFileSync('users.json', '\u003A');
+            fs.appendFileSync('users.json', '\n');
+            fs.appendFileSync('users.json', JSON.stringify(name, null, 5));}
+        else{
+        	var name = {'username': person['username'], 'password' : person['password']}
+            //var myObj = JSON.stringify(name);
+            fs.appendFileSync('users.json', JSON.stringify(person['username']));
+            fs.appendFileSync('users.json', '\u003A');
+            fs.appendFileSync('users.json', '\n');
+            fs.appendFileSync('users.json', JSON.stringify(name, null, 5));
+            fs.appendFileSync('users.json', '\u002C');
+            fs.appendFileSync('users.json', '\n');
+
+        }
+    }
+    fs.appendFileSync('users.json', '\n');
+    fs.appendFileSync('users.json', "}");
+    return "Cadastro efetuado com sucesso!";
+}
