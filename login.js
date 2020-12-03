204b01813e847fb458b951c5442ae30f53230556
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2020-12-03 14:51:55 -0300

    Update login.js

--- login.js
+++ login.js
@@ -4,7 +4,6 @@ exports.login = function(username, password) {
     let rawdata = fs.readFileSync('users.json');
     let creds = JSON.parse(rawdata);
     let users = Object.keys(creds);
-    console.log(rawdata);
     for(var i = 0; i < users.length; i++) {
         var person = creds[users[i]];
         if(person['username'] === username && person['password'] === password) {
